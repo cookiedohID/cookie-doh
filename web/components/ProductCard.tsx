80f81d0d2e9aa1b576afec51650ee8dc44aeb621
Angel Tan <angeltan@Angels-iMac-2.local>
2026-02-03 20:22:25 +0700

    a

--- web/components/ProductCard.tsx
+++ web/components/ProductCard.tsx
@@ -67,10 +67,10 @@ export default function ProductCard({
           <h3 className={styles.title}>{flavor.name}</h3>
         </div>
 
-{/* Tagline */}
-<p className={styles.tagline}>
-  {flavor.description ? flavor.description : "TAGLINE_MISSING"}
-</p>
+{flavor.description ? (
+  <p className={styles.tagline}>{flavor.description}</p>
+) : null}
+
 
 
         {flavor.description ? (
